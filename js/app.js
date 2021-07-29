da857b56e3733a42bd8b85c16e4683201b8b2493
Ahmadreza Mozaffary <am2002.de@gmail.com>
2021-07-29 20:01:25 +0430

    Major update (add js,...)

---
+++ js/app.js
@@ -0,0 +1,150 @@
+const dice = document.querySelector("img");
+const score = [0, 0];
+const player0Title = document.querySelector(".player-title0");
+const player1Title = document.querySelector(".player-title1");
+const currentTitle0 = document.querySelector(".current-title0");
+const currentTitle1 = document.querySelector(".current-title1");
+const player0El = document.querySelector(".player0");
+const player1El = document.querySelector(".player1");
+const currentScore0 = document.querySelector(".current-score0");
+const currentScore1 = document.querySelector(".current-score1");
+const totalScore0El = document.querySelector(".total0");
+const totalScore1El = document.querySelector(".total1");
+const langMessage = document.querySelector(".langmsg");
+
+let winMessage = "You win!";
+const langFA = " 🇮🇷 زبان به فارسی تغییر کرد ";
+const langEN = "Language changed to EN 🇺🇸";
+
+const btnRoll = document.querySelector(".roll");
+const btnHold = document.querySelector(".hold");
+const btnReset = document.querySelector(".reset");
+const btnLanguage = document.querySelector(".lang");
+const btnHelp = document.querySelector(".help");
+const btnX = document.querySelector(".x-button");
+
+const switchPlayer = function () {
+  player0El.classList.toggle("active-player");
+  player1El.classList.toggle("active-player");
+  player0El.classList.toggle("deactive-player");
+  player1El.classList.toggle("deactive-player");
+};
+
+const changeModalDisplay = function (display) {
+  document.querySelector(".modal-window").style.display = display;
+};
+
+let play = true;
+let currentScore = 0;
+let activePlayer = 0;
+
+btnRoll.addEventListener("click", () => {
+  if (play) {
+    const diceNumber = Math.trunc(Math.random() * 6) + 1;
+    dice.src = `./images/dice${diceNumber}.png`;
+    dice.classList.remove("hidden");
+    if (score[0] <= 100 && score[1] <= 100) {
+      if (activePlayer === 0) {
+        currentScore += diceNumber;
+        currentScore0.textContent = currentScore;
+        if (diceNumber === 1) {
+          currentScore = 0;
+          activePlayer = 1;
+          currentScore0.textContent = 0;
+          switchPlayer();
+        }
+      } else {
+        currentScore += diceNumber;
+        currentScore1.textContent = currentScore;
+        if (diceNumber === 1) {
+          currentScore = 0;
+          activePlayer = 0;
+          currentScore1.textContent = 0;
+          switchPlayer();
+        }
+      }
+    } else {
+      play = false;
+      dice.classList.add("hidden");
+      if (score[0] >= 99) {
+        player0Title.textContent = winMessage;
+        switchPlayer();
+      } else if (score[1] >= 99) {
+        player1Title.textContent = winMessage;
+        switchPlayer();
+      }
+    }
+  }
+});
+
+btnHold.addEventListener("click", () => {
+  if (play) {
+    if (score[0] <= 100 && score[1] <= 100) {
+      if (activePlayer === 0) {
+        score[0] += currentScore;
+        totalScore0El.textContent = score[0];
+        activePlayer = 1;
+        currentScore0.textContent = 0;
+        currentScore = 0;
+        switchPlayer();
+      } else {
+        score[1] += currentScore;
+        totalScore1El.textContent = score[1];
+        activePlayer = 0;
+        currentScore1.textContent = 0;
+        currentScore = 0;
+        switchPlayer();
+      }
+    }
+  } else {
+  }
+});
+
+btnReset.addEventListener("click", () => {
+  play = true;
+  activePlayer = 0;
+  currentScore = 0;
+  score[0] = score[1] = 0;
+  currentScore0.textContent = 0;
+  currentScore1.textContent = 0;
+  totalScore0El.textContent = 0;
+  totalScore1El.textContent = 0;
+  player0Title.textContent = "Player 1";
+  player1Title.textContent = "Player 2";
+  player0El.classList.add("active-player");
+  player0El.classList.remove("deactive-player");
+  player1El.classList.add("deactive-player");
+  player1El.classList.remove("active-player");
+  dice.classList.add("hidden");
+});
+
+btnLanguage.addEventListener("click", () => {
+  winMessage = winMessage === "You win!" ? "شما برنده شدید" : "You win!";
+  const isTrue = winMessage === "You win!";
+  const msg = isTrue ? langFA : langEN;
+  langMessage.textContent = msg;
+  player0Title.textContent = isTrue ? "بازیکن ۱" : "Player 1";
+  player1Title.textContent = isTrue ? "بازیکن ۲" : "Player 2";
+  currentTitle0.textContent = isTrue ? "امتیاز فعلی" : "Current";
+  currentTitle1.textContent = isTrue ? "امتیاز فعلی" : "Current";
+  btnRoll.textContent = isTrue ? "بریز 🖲" : "Roll 🖲";
+  btnHold.textContent = isTrue ? "نگهداری 📥" : "Hold 📥";
+  btnReset.textContent = isTrue ? "از اول ♻️" : "Reset ♻️";
+  btnLanguage.textContent = isTrue ? "فا / EN" : "Fa / En";
+  btnHelp.textContent = isTrue ? "راهنما" : "Help";
+});
+
+btnHelp.addEventListener("click", () => {
+  changeModalDisplay("flex");
+  if (btnHelp.textContent === "Help") {
+    document.querySelector(".modal-content-en").style.display = "block";
+    document.querySelector(".modal-content-fa").style.display = "none";
+  } else {
+    document.querySelector(".modal-content-en").style.display = "none";
+    document.querySelector(".modal-content-fa").style.display = "block";
+  }
+});
+
+btnX.addEventListener("click", () => {
+  changeModalDisplay("none");
+});
