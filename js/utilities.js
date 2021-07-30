15cafe887b9d942e9c3306e58b0b834d8a9657bf
RezaKargar <r.kargar.2014@gmail.com>
2021-07-30 17:31:56 +0430

    Fix language selection UI problems
    1. There was some problems in logic and UI of language selection
    section which are now got fixed. The problem was about `WinMessage`,
    WinMessage's value must have been changed after the checking the
    current language
    
    2. The functionality of fading out an element added by implementing
    the `utilities.js` file and this function used to fade out the message
    of language change

---
+++ js/utilities.js
@@ -0,0 +1,23 @@
+/**
+ * Fades out an element in after a period of time
+ * @param {Node} element - The element that will be faded out
+ * @param {Number} [startFadingOutAfter = 2000] - The time that fading out will be started after
+ * @param {Number} [speed = 1] - The speed of fading out, speed must be greater than zero.
+ */ 
+function fadeOut(element, startFadingOutAfter = 2000, speed = 1) {
+    // speed must be greater than zero
+    speed = speed <= 0 ? 1 : speed;
+
+    setTimeout(function() { 
+        var timerId = setInterval(function() { 
+          var opacity = element.style.opacity; 
+          if (opacity == 0) { 
+            clearInterval(timerId);
+          } else {
+            element.style.opacity = opacity - 0.05;
+          }
+        }, 100 / speed);
+      }, startFadingOutAfter);
+    
+      element.style.opacity = 1; 
+}
